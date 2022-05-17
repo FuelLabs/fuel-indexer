bcedec50eda67139ef61c51216c5f08e737b24bb
rashad <spam.rashad@protonmail.com>
2022-05-17 08:34:56 -0700

    Copy over verify_tag logic (#25)
    Co-authored-by: Rashad Alston <rashad@Rashads-MacBook-Pro.local>

---
+++ .github/workflows/scripts/verify_tag.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+# References https://github.com/FuelLabs/fuel-core/blob/master/.github/workflows/scripts/verify_tag.sh
+set -e
+
+err() {
+    echo -e "\e[31m\e[1merror:\e[0m $@" 1>&2;
+}
+
+status() {
+    WIDTH=12
+    printf "\e[32m\e[1m%${WIDTH}s\e[0m %s\n" "$1" "$2"
+}
+
+REF=$1
+MANIFEST=$2
+
+if [ -z "$REF" ]; then
+    err "Expected ref to be set"
+    exit 1
+fi
+
+if [ -z "$MANIFEST" ]; then
+    err "Expected manifest to be set"
+    exit 1
+fi
+
+# strip preceeding 'v' if it exists on tag
+REF=${REF/#v}
+TOML_VERSION=$(cat $MANIFEST | dasel -r toml 'package.version')
+
+if [ "$TOML_VERSION" != "$REF" ]; then
+    err "Crate version $TOML_VERSION, doesn't match tag version $REF"
+    exit 1
+else
+  status "Crate version matches tag $TOML_VERSION"
+fi
