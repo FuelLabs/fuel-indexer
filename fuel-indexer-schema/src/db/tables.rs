914c8c60619470b1b24f9b8deeb30a3481fe2ba4
Rashad Alston <rashad@Rashads-MacBook-Pro.local>
2022-08-24 11:29:27 -0700

    Add tests

--- fuel-indexer-schema/src/db/tables.rs
+++ fuel-indexer-schema/src/db/tables.rs
@@ -402,7 +402,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_schema_builder_for_basic_schema_returns_proper_create_sql() {
+    fn test_schema_builder_for_basic_postgres_schema_returns_proper_create_sql() {
         let graphql_schema: &str = r#"
         schema {
             query: QueryRoot
@@ -454,7 +454,7 @@ mod tests {
     }
 
     #[test]
-    fn test_schema_builder_for_indices_returns_proper_create_sql() {
+    fn test_schema_builder_for_postgres_indices_returns_proper_create_sql() {
         let graphql_schema: &str = r#"
         schema {
             query: QueryRoot
@@ -493,7 +493,7 @@ mod tests {
     }
 
     #[test]
-    fn test_schema_builder_for_foreign_keys_returns_proper_create_sql() {
+    fn test_schema_builder_for_postgres_foreign_keys_returns_proper_create_sql() {
         let graphql_schema: &str = r#"
         schema {
             query: QueryRoot
@@ -532,5 +532,86 @@ mod tests {
         assert_eq!(foreign_keys.len(), 2);
         assert_eq!(foreign_keys[0].create_statement(), "ALTER TABLE namespace.lender ADD CONSTRAINT fk_borrower_id FOREIGN KEY (borrower) REFERENCES namespace.borrower(id) ON DELETE NO ACTION ON UPDATE NO ACTION INITIALLY DEFERRED;".to_string());
         assert_eq!(foreign_keys[1].create_statement(), "ALTER TABLE namespace.auditor ADD CONSTRAINT fk_borrower_id FOREIGN KEY (borrower) REFERENCES namespace.borrower(id) ON DELETE NO ACTION ON UPDATE NO ACTION INITIALLY DEFERRED;".to_string());
+    }
+
+    #[test]
+    fn test_schema_builder_for_sqlite_indices_returns_proper_create_sql() {
+        let graphql_schema: &str = r#"
+        schema {
+            query: QueryRoot
+        }
+
+        type QueryRoot {
+            thing1: Thing1
+            thing2: Thing2
+        }
+
+        type Payer {
+            id: ID!
+            account: Address! @indexed
+        }
+
+        type Payee {
+            id: ID!
+            account: Address!
+            hash: Bytes32! @indexed
+        }
+    "#;
+
+        let sb = SchemaBuilder::new("namespace", "v1", DbType::Sqlite);
+
+        let SchemaBuilder { indices, .. } = sb.build(graphql_schema);
+
+        assert_eq!(indices.len(), 2);
+        assert_eq!(
+            indices[0].create_statement(),
+            "CREATE INDEX payer_account_idx ON payer(account);".to_string()
+        );
+        assert_eq!(
+            indices[1].create_statement(),
+            "CREATE INDEX payee_hash_idx ON payee(hash);".to_string()
+        );
+    }
+
+    #[test]
+    fn test_schema_builder_for_sqlite_foreign_keys_returns_proper_create_sql() {
+        let graphql_schema: &str = r#"
+        schema {
+            query: QueryRoot
+        }
+
+        type QueryRoot {
+            borrower: Borrower
+            lender: Lender
+            auditor: Auditor
+        }
+
+        type Borrower {
+            id: ID!
+            account: Address! @indexed
+        }
+
+        type Lender {
+            id: ID!
+            account: Address!
+            hash: Bytes32! @indexed
+            borrower: Borrower!
+        }
+
+        type Auditor {
+            id: ID!
+            account: Address!
+            hash: Bytes32! @indexed
+            borrower: Borrower!
+        }
+    "#;
+
+        let sb = SchemaBuilder::new("namespace", "v1", DbType::Sqlite);
+
+        let SchemaBuilder { foreign_keys, .. } = sb.build(graphql_schema);
+
+        assert_eq!(foreign_keys.len(), 2);
+        assert_eq!(foreign_keys[0].create_statement(), "ALTER TABLE lender DROP COLUMN borrower; ALTER TABLE lender ADD COLUMN borrower BIGINT REFERENCES borrower(id);");
+        assert_eq!(foreign_keys[1].create_statement(), "ALTER TABLE auditor DROP COLUMN borrower; ALTER TABLE auditor ADD COLUMN borrower BIGINT REFERENCES borrower(id);");
     }
 }
