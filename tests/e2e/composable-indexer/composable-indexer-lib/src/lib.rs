821bb878b92338fe7bd6105c066a98dcfa39fe63
TJ Sharp <tj.tommyj@gmail.com>
2022-09-01 11:36:18 -0400

    Fixes

--- tests/e2e/composable-indexer/composable-indexer-lib/src/lib.rs
+++ tests/e2e/composable-indexer/composable-indexer-lib/src/lib.rs
@@ -1,29 +1,28 @@
 extern crate alloc;
-use fuel_indexer_macros::{graphql_schema, handler};
-use fuel_indexer_schema::Bytes32;
-use fuels_abigen_macro::wasm_abigen;
+use fuel_indexer_macros::indexer;
 
 // IMPORTANT: This must match the namespace in your
-graphql_schema!("composability_test", "schema/schema.graphql");
-wasm_abigen!(
-    no_name,
-    "tests/e2e/composable-indexer/composable-indexer-lib/contracts/ping/out/debug/ping-abi.json"
-);
 
-#[handler]
-fn function_one(ping: Ping) {
-    Logger::info("function_one handling a Pong event.");
+#[indexer(
+    abi = "tests/e2e/composable-indexer/composable-indexer-lib/contracts/ping/out/debug/ping-abi.json",
+    namespace = "composability_test",
+    schema = "schema/schema.graphql",
+)]
+mod composability_test {
+    fn function_one(ping: Ping) {
+        Logger::info("function_one handling a Pong event.");
 
-    let mut bytes: [u8; 32] = [0u8; 32];
+        let mut bytes: [u8; 32] = [0u8; 32];
 
-    bytes.copy_from_slice(&ping.message.as_bytes()[..32]);
+        bytes.copy_from_slice(&ping.message.as_bytes()[..32]);
 
-    let entity = Message {
-        id: ping.id,
-        ping: ping.value,
-        pong: 456,
-        message: Bytes32::from(bytes),
-    };
+        let entity = Message {
+            id: ping.id,
+            ping: ping.value,
+            pong: 456,
+            message: Bytes32::from(bytes),
+        };
 
-    entity.save();
+        entity.save();
+    }
 }
