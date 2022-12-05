73f471ab9809dcac2105e045b44191eacf9564c0
rashad <spam.rashad@protonmail.com>
2022-12-05 18:13:36 -0500

    add sqlite migration script (#399)
    Co-authored-by: Rashad Alston <rashad@Rashads-MacBook-Pro.local>

--- scripts/utils/refresh_test_db.bash
+++ scripts/utils/refresh_test_db.bash
@@ -7,8 +7,18 @@
 
 set -x
 
-dropdb postgres
-createdb postgres
-DATABASE_URL=postgres://postgres@127.0.0.1 bash scripts/run_migrations.bash
+db_type="${1}"
+
+if [ $db_type == "postgres" ]; then
+    dropdb postgres
+    createdb postgres
+    DATABASE_URL=postgres://postgres@127.0.0.1 bash scripts/run_migrations.bash
+elif [ $db_type == "sqlite" ]; then
+    rm -fv $PWD/packages/fuel-indexer-tests/test.db
+    DATABASE_URL=sqlite://$PWD/packages/fuel-indexer-tests/test.db bash scripts/run_migrations.bash
+else
+    echo "Invalid db param. Expected 'sqlite' or 'postgres'. Found '$db_type'"
+    exit 1
+fi
 
 set +x
