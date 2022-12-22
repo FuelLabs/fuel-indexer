3914dc1e65a128e60bd28e78a0a1658cd86282ab
Alexander Decurnou <alex@deekerno.com>
2022-12-22 13:20:34 -0500

    Add web API postgres test to ensure that assets are registered

--- packages/fuel-indexer-tests/tests/integration/web_api_postgres.rs
+++ packages/fuel-indexer-tests/tests/integration/web_api_postgres.rs
@@ -1,10 +1,15 @@
 use fuel_indexer_api_server::api::GraphQlApi;
 use fuel_indexer_lib::config::GraphQLConfig;
 use fuel_indexer_postgres as postgres;
+use fuel_indexer_tests::assets::{
+    SIMPLE_WASM_MANIFEST, SIMPLE_WASM_SCHEMA, SIMPLE_WASM_WASM,
+};
 use fuel_indexer_tests::fixtures::{
     api_server_app_postgres, http_client, indexer_service_postgres,
     postgres_connection_pool,
 };
+use hyper::header::{AUTHORIZATION, CONTENT_TYPE};
+use reqwest::{multipart, Body};
 use tokio::task::spawn;
 
 #[tokio::test]
@@ -90,4 +95,55 @@ async fn test_database_postgres_metrics_properly_increments_counts_when_queries_
             .unwrap()
             >= 1,
     );
+}
+
+#[tokio::test]
+#[cfg(all(feature = "postgres"))]
+async fn test_asset_upload_endpoint_properly_adds_assets_to_database_postgres() {
+    let _ = indexer_service_postgres().await;
+    let app = api_server_app_postgres().await;
+
+    let server = axum::Server::bind(&GraphQLConfig::default().into())
+        .serve(app.into_make_service());
+
+    let server_handle = tokio::spawn(server);
+
+    let pool = postgres_connection_pool().await;
+    let mut conn = pool.acquire().await.unwrap();
+    let is_index_registered =
+        postgres::index_is_registered(&mut conn, "fuel_indexer_test", "index1")
+            .await
+            .unwrap();
+    assert!(is_index_registered.is_none());
+
+    let manifest_file =
+        multipart::Part::stream(SIMPLE_WASM_MANIFEST).file_name("simple_wasm.yaml");
+    let schema_file =
+        multipart::Part::stream(SIMPLE_WASM_SCHEMA).file_name("simple_wasm.graphql");
+    let wasm_file =
+        multipart::Part::stream(SIMPLE_WASM_WASM).file_name("simple_wasm.wasm");
+
+    let form = multipart::Form::new()
+        .part("manifest", manifest_file)
+        .part("schema", schema_file)
+        .part("wasm", wasm_file);
+
+    let client = http_client();
+    let resp = client
+        .post("http://127.0.0.1:29987/api/index/fuel_indexer_test/index1")
+        .multipart(form)
+        .header(CONTENT_TYPE, "multipart/form-data".to_owned())
+        .header(AUTHORIZATION, "foo".to_owned())
+        .send()
+        .await
+        .unwrap();
+
+    assert!(resp.status().is_success());
+
+    let is_index_registered =
+        postgres::index_is_registered(&mut conn, "fuel_indexer_test", "index1")
+            .await
+            .unwrap();
+
+    assert!(is_index_registered.is_some());
 }
