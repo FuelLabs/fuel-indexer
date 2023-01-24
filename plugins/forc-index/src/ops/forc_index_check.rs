b3e091ad3c6d2e7dbb5b328053f2cce97cdf5fe2
Rashad Alston <rashad@Rashads-MacBook-Pro.local>
2023-01-24 16:04:49 -0500

    chore: simplify forc-index check

--- plugins/forc-index/src/ops/forc_index_check.rs
+++ plugins/forc-index/src/ops/forc_index_check.rs
@@ -4,6 +4,10 @@ use serde_json::{to_string_pretty, value::Value, Map};
 use std::process::Command;
 use tracing::error;
 
+const MESSAGE_PADDING: usize = 64;
+const EMOJI_PADDING: usize = 3;
+const HEADER_PADDING: usize = 20;
+
 fn center_align(s: &str, n: usize) -> String {
     format!("{: ^width$}", s, width = n)
 }
@@ -14,12 +18,9 @@ fn rightpad_whitespace(s: &str, n: usize) -> String {
 
 fn format_exec_msg(exec_name: &str, path: Option<String>) -> String {
     if path.is_some() {
-        rightpad_whitespace(
-            &format!("  Found '{}' at '{}'", exec_name, path.unwrap()),
-            76,
-        )
+        rightpad_whitespace(&format!("{}", path.unwrap()), MESSAGE_PADDING)
     } else {
-        rightpad_whitespace(&format!("Could not located '{}'", exec_name), 76)
+        rightpad_whitespace(&format!("Can't locate {}", exec_name), MESSAGE_PADDING)
     }
 }
 
@@ -35,11 +36,11 @@ fn find_executable(exec_name: &str) -> (String, Option<String>) {
             let path = String::from_utf8_lossy(&o.stdout)
                 .strip_suffix('\n')
                 .map(|x| x.to_string());
-            (center_align("✅", 3), path)
+            (center_align("✅", EMOJI_PADDING), path)
         }
         Err(e) => {
-            error!("  Could not locate {}: {}", exec_name, e);
-            (center_align("⛔️", 3), None)
+            error!("Can't locate {}: {}", exec_name, e);
+            (center_align("⛔️", EMOJI_PADDING), None)
         }
     }
 }
@@ -73,35 +74,43 @@ fn find_indexer_service_info(cmd: &CheckCommand) -> (String, String) {
     {
         Ok(o) => {
             let (emoji, msg) = match String::from_utf8_lossy(&o.stdout)
-            .to_string()
-            .strip_suffix('\n')  {
-            Some(pid) => (
-                center_align("✅", 3),
-                center_align(&format!(
-                    "Local fuel-indexer service found: PID({}) | Port({})",
-                    &pid, &cmd.grpahql_api_port
-                ), 76)
-            ),
-            None => (
-                center_align("⛔️", 6),
-                center_align(&format!(
-                    "Failed to detect a locally running fuel-indexer service at Port({}).",
-                    cmd.grpahql_api_port
-                ), 76))
-        };
+                .to_string()
+                .strip_suffix('\n')
+            {
+                Some(pid) => (
+                    center_align("✅", EMOJI_PADDING),
+                    rightpad_whitespace(
+                        &format!(
+                            "Local service found: PID({}) | Port({}).",
+                            &pid, &cmd.grpahql_api_port
+                        ),
+                        MESSAGE_PADDING,
+                    ),
+                ),
+                None => (
+                    center_align("⛔️", EMOJI_PADDING + 3),
+                    rightpad_whitespace(
+                        &format!(
+                            "Failed to detect service at Port({}).",
+                            cmd.grpahql_api_port
+                        ),
+                        MESSAGE_PADDING,
+                    ),
+                ),
+            };
 
             (emoji, msg)
         }
         Err(e) => {
-            error!("Could not find info for fuel-indexer service: {}", e);
+            error!("Could not find info for fuel-indexer service:\n'{}'", e);
             (
-                center_align("⛔️", 3),
-                center_align(
+                center_align("⛔️", EMOJI_PADDING + 3),
+                rightpad_whitespace(
                     &format!(
-                "Failed to detect a locally running fuel-indexer service at port: {}.",
-                cmd.grpahql_api_port
-            ),
-                    76,
+                        "Failed to detect service at Port({}).",
+                        cmd.grpahql_api_port
+                    ),
+                    MESSAGE_PADDING,
                 ),
             )
         }
@@ -138,12 +147,12 @@ pub fn init(command: CheckCommand) -> anyhow::Result<()> {
                 .expect("Failed to read JSON response.");
 
             println!(
-                "\n✅ Sucessfully retrieved indexer service health:\n\n{}",
+                "\n✅ Sucessfully fetched service health:\n\n{}",
                 to_string_pretty(&res_json).unwrap()
             );
         }
         Err(e) => {
-            error!("\n❌ Could not connect to indexer service: {}", e);
+            error!("\n❌ Could not connect to indexer service:\n'{}'", e);
         }
     }
 
@@ -160,39 +169,41 @@ pub fn init(command: CheckCommand) -> anyhow::Result<()> {
     let (wasm_snip_emoji, _wasm_snip_path, wasm_snip_msg) =
         find_executable_with_msg(wasm_snip);
 
-    let details_header = center_align("Details", 76);
-    let check_header = center_align("Component", 20);
-    let status_headers = center_align("Status", 5);
-    let binary_header = rightpad_whitespace("fuel-indexer binary", 20);
-    let service_header = rightpad_whitespace("fuel-indexer service", 20);
-    let psql_header = rightpad_whitespace(psql, 20);
-    let sqlite_header = rightpad_whitespace(sqlite, 20);
-    let fuel_core_header = rightpad_whitespace(fuel_core, 20);
-    let docker_header = rightpad_whitespace(docker, 20);
-    let fuelup_header = rightpad_whitespace(fuelup, 20);
-    let wasm_snip_header = rightpad_whitespace(wasm_snip, 20);
+    // Padding here is done on an as-needed basis
+    let status_padding = 5;
+    let details_header = center_align("Details", MESSAGE_PADDING + 2);
+    let check_header = center_align("Component", HEADER_PADDING);
+    let status_headers = center_align("Status", status_padding);
+    let binary_header = rightpad_whitespace("fuel-indexer binary", HEADER_PADDING);
+    let service_header = rightpad_whitespace("fuel-indexer service", HEADER_PADDING);
+    let psql_header = rightpad_whitespace(psql, HEADER_PADDING);
+    let sqlite_header = rightpad_whitespace(sqlite, HEADER_PADDING);
+    let fuel_core_header = rightpad_whitespace(fuel_core, HEADER_PADDING);
+    let docker_header = rightpad_whitespace(docker, HEADER_PADDING);
+    let fuelup_header = rightpad_whitespace(fuelup, HEADER_PADDING);
+    let wasm_snip_header = rightpad_whitespace(wasm_snip, HEADER_PADDING);
 
     let stdout = format!(
         r#"
-+--------+------------------------+----------------------------------------------------------------------------+
++--------+------------------------+------------------------------------------------------------------+
 | {status_headers} |  {check_header}  |{details_header}|
-+--------+------------------------+----------------------------------------------------------------------------+
-|  {indexer_emoji}  | {binary_header}   |{indexer_msg}|
-+--------+------------------------+----------------------------------------------------------------------------+
-| {service_emoji} | {service_header}   |{service_msg}|
-+--------+------------------------+----------------------------------------------------------------------------+
-|  {psql_emoji}  | {psql_header}   |{psql_msg}|
-+--------+------------------------+----------------------------------------------------------------------------+
-|  {sqlite_emoji}  | {sqlite_header}   |{sqlite_msg}|
-+--------+------------------------+----------------------------------------------------------------------------+
-|  {fuel_core_emoji}  | {fuel_core_header}   |{fuel_core_msg}|
-+--------+------------------------+----------------------------------------------------------------------------+
-|  {docker_emoji}  | {docker_header}   |{docker_msg}|
-+--------+------------------------+----------------------------------------------------------------------------+
-|  {fuelup_emoji}  | {fuelup_header}   |{fuelup_msg}|
-+--------+------------------------+----------------------------------------------------------------------------+
-|  {wasm_snip_emoji}  | {wasm_snip_header}   |{wasm_snip_msg}|
-+--------+------------------------+----------------------------------------------------------------------------+
++--------+------------------------+------------------------------------------------------------------+
+|  {indexer_emoji}  | {binary_header}   |  {indexer_msg}|
++--------+------------------------+------------------------------------------------------------------+
+| {service_emoji} | {service_header}   |  {service_msg}|
++--------+------------------------+------------------------------------------------------------------+
+|  {psql_emoji}  | {psql_header}   |  {psql_msg}|
++--------+------------------------+------------------------------------------------------------------+
+|  {sqlite_emoji}  | {sqlite_header}   |  {sqlite_msg}|
++--------+------------------------+------------------------------------------------------------------+
+|  {fuel_core_emoji}  | {fuel_core_header}   |  {fuel_core_msg}|
++--------+------------------------+------------------------------------------------------------------+
+|  {docker_emoji}  | {docker_header}   |  {docker_msg}|
++--------+------------------------+------------------------------------------------------------------+
+|  {fuelup_emoji}  | {fuelup_header}   |  {fuelup_msg}|
++--------+------------------------+------------------------------------------------------------------+
+|  {wasm_snip_emoji}  | {wasm_snip_header}   |  {wasm_snip_msg}|
++--------+------------------------+------------------------------------------------------------------+
 "#
     );
 
