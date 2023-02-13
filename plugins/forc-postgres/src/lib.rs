9bff898f6e4531309ab55ebbf3fe64793bb3eccb
Rashad Alston <rashad@Rashads-MBP.lan>
2023-02-12 20:15:07 -0500

    readd forc-postgres

--- plugins/forc-postgres/src/lib.rs
+++ plugins/forc-postgres/src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod cli;
+pub mod commands;
+pub mod ops;
+pub(crate) mod pg;
+pub(crate) mod utils;
