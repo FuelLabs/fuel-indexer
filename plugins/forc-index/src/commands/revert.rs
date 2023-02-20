a12ce0e63b490dd7641fec5cfcb621d1264d1c2e
rvmelkonian <r.v.melkonian@gmail.com>
2023-02-20 17:16:40 +0000

    create initial outline

---
+++ plugins/forc-index/src/commands/revert.rs
@@ -0,0 +1,33 @@
+use crate{ops::forc_index_revert, utils::defaults};
+use anyhow::Result;
+use clap::Parser;
+use std::path::PathBuf;
+
+/// Revert the running index to the previous version.
+#[derive(Debug, Parser)]
+pub struct Revert{
+    /// URL at which index is deployed.
+    #[clap(long, default_value = defaults::INDEXER_SERVICE_HOST, help = "URL at which index is deployed.")]
+    pub url: String,
+
+    /// Path to the manifest of the indexer project being reverted. 
+    #[clap(
+        short,
+        long,
+        help = "Path to the manifest of indexer project being deployed."
+    )]
+    pub manifest: Option<String>,
+
+    /// Path of index project.
+    #[clap(short, long, help = "Path to the indexer project.")]
+    pub path: Option<PathBuf>,
+
+    /// Authentication header value.
+    #[clap(long, help = "Authentication header value.")]
+    pub auth: Option<String>,
+}
+
+pub fn exec(command: Command) -> Result<()> {
+    forc_index_revert::init(command)?;
+    Ok(())
+}
