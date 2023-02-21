2a8dee4fe676101453354417cec96da8449bce9e
rvmelkonian <r.v.melkonian@gmail.com>
2023-02-21 14:01:45 +0000

    remove pseudo code

--- plugins/forc-index/src/ops/forc_index_revert.rs
+++ plugins/forc-index/src/ops/forc_index_revert.rs
@@ -1,16 +1,5 @@
 use crate::cli::RevertCommand;
 
-
 pub fn init(command: RevertCommand) -> anyhow::Result<()> {
-    let manifest = command.manifest.unwrap_or_else(|| {
-        let mut path = command.path.unwrap_or_else(|| PathBuf::from("."));
-        path.push("Cargo.toml");
-        path.to_str().unwrap().to_string()
-    });
-    let manifest = Manifest::from_path(&manifest)?;
-    let indexer = Indexer::from_manifest(&manifest)?;
-    let indexer = indexer.revert()?;
-    let indexer = indexer.deploy()?;
-    indexer.wait_for_ready()?;
     Ok(())
 }
