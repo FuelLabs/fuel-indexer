6878d4e0924bf5d921a4351cd51ca8530c064118
rvmelkonian <r.v.melkonian@gmail.com>
2023-02-24 18:25:12 +0000

    update path

--- scripts/utils/clean_and_test.bash
+++ scripts/utils/clean_and_test.bash
@@ -4,4 +4,4 @@ set -e  # exit if any command fails
 
 bash scripts/utils/refresh_test_db.bash
 bash scripts/utils/build_test_wasm_module.bash
-bash scripts/utils/run_tests.bash
+bash packages/fuel-indexer-tests/scripts/e2e.bash
