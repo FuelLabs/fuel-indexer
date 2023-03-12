a20a6d325b8a8104e9f2bbde37dceae7337a8e14
rvmelkonian <r.v.melkonian@gmail.com>
2023-03-12 13:17:35 +0000

    change formatting

--- plugins/forc-index/src/ops/forc_index_tree.rs
+++ plugins/forc-index/src/ops/forc_index_tree.rs
@@ -1,4 +1,5 @@
 use crate::cli::TreeCommand;
+use owo_colors::OwoColorize;
 use std::path::Path;
 use tracing::info;
 use walkdir::WalkDir;
@@ -6,7 +7,7 @@ use walkdir::WalkDir;
 pub fn init(command: TreeCommand) -> anyhow::Result<()> {
     let current_dir = Path::new(".");
 
-    //@TODO add in verbose details by file type, 
+    //@TODO add in verbose details by file type,
     //@TODO colourize
     for entry in WalkDir::new(current_dir) {
         let entry = entry?;
@@ -15,12 +16,12 @@ pub fn init(command: TreeCommand) -> anyhow::Result<()> {
 
         // Indent the output based on the depth of the directory.
         // Each level of indentation is two spaces.
-        let indent = "  ".repeat(depth);
+        let indent = "  |".repeat(depth);
 
         if path.is_dir() {
-            info!("{}{}/", indent, path.display());
+            info!("{}{}/", indent.bright_cyan(), path.display().bright_cyan());
         } else {
-            info!("{}{}", indent, path.display());
+            info!("{}{}", indent.bright_cyan(), path.display().bright_cyan());
         }
     }
     Ok(())
