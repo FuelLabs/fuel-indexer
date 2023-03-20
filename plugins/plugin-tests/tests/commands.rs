ca26cfe75d6cab946042ce8ffd13b8a35047f109
rvmelkonian <r.v.melkonian@gmail.com>
2023-03-20 14:25:57 +0000

    use Builder for dir name, passing build and init tests

--- plugins/plugin-tests/tests/commands.rs
+++ plugins/plugin-tests/tests/commands.rs
@@ -4,19 +4,11 @@ use forc_index::commands::{
 use std::fs::{self, ReadDir};
 use std::io;
 use std::path::{Path, PathBuf};
-use tempfile::TempDir;
+use tempfile::{Builder, TempDir};
 
 #[test]
 fn init_command_creates_correct_project_tree() {
-    let temp_dir = TempDir::new().expect("Failed to create temp dir");
-    let temp_dir_path = temp_dir.path().to_path_buf();
-    let temp_dir_name = temp_dir_path
-        .file_name()
-        .unwrap()
-        .to_str()
-        .unwrap()
-        .trim_start_matches('.')
-        .to_string();
+    let (_temp_dir, temp_dir_path, temp_dir_name) = init_temp_dir();
 
     let command = InitCommand {
         name: Some(temp_dir_name.clone()),
@@ -28,11 +20,11 @@ fn init_command_creates_correct_project_tree() {
 
     forc_index::commands::init::exec(command).expect("Init command failed");
 
-    let manifest_path = temp_dir_name.clone() + ".manifest.yaml";
+    let manifest = temp_dir_name.clone() + ".manifest.yaml";
     let schema_path = format!("schema/{}.schema.graphql", temp_dir_name);
 
     let expected_project_tree = vec![
-        manifest_path.as_str(),
+        manifest.as_str(),
         "Cargo.toml",
         "src",
         "src/lib.rs",
@@ -54,30 +46,35 @@ fn init_command_creates_correct_project_tree() {
 
 #[test]
 fn build_command_creates_artifact_at_expected_path() {
-    let temp_dir_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("output");
-    remove_contents_of_dir(&temp_dir_path).expect("Failed to remove output directory");
+    let (_temp_dir, temp_dir_path, temp_dir_name) = init_temp_dir();
 
-    //we set the namespace to the name of the dir so that the
-    //build command can find the manifest
     let command = InitCommand {
-        name: Some("output".to_string()),
+        name: Some(temp_dir_name.clone()),
         path: Some(temp_dir_path.clone()),
-        namespace: "output".to_string(),
+        namespace: temp_dir_name.to_string(),
         native: false,
         absolute_paths: false,
     };
 
     forc_index::commands::init::exec(command).expect("Init command failed");
 
-    assert!(
-        Path::new(&temp_dir_path.join("output.manifest.yaml")).exists(),
-        "Manifest not found at expected path"
-    );
-
     println!("init success");
 
-    //cd into the output dir
+    //cd into the test indexer project temp dir
     std::env::set_current_dir(&temp_dir_path).expect("Failed to set current dir");
+
+    //convert temp_dir_name from camelCase to snake-case
+    let temp_dir_name = temp_dir_name
+        .chars()
+        .map(|c| {
+            if c.is_uppercase() {
+                format!("_{}", c.to_lowercase())
+            } else {
+                c.to_string()
+            }
+        })
+        .collect::<String>();
+    let manifest = temp_dir_name.clone() + ".manifest.yaml";
 
     let command = BuildCommand {
         target: None,
@@ -87,7 +84,7 @@ fn build_command_creates_artifact_at_expected_path() {
         profile: None,
         release: false,
         locked: false,
-        manifest: None,
+        manifest: Some(manifest),
         output_dir_root: None,
     };
 
@@ -101,23 +98,17 @@ fn build_command_creates_artifact_at_expected_path() {
     );
 }
 
-fn remove_contents_of_dir(path: &Path) -> io::Result<()> {
-    if path.is_dir() {
-        let entries: ReadDir = fs::read_dir(path)?;
-        entries.for_each(|entry| {
-            if let Ok(entry) = entry {
-                let entry_path = entry.path();
-                if entry_path.is_dir() {
-                    if let Err(e) = fs::remove_dir_all(&entry_path) {
-                        eprintln!("Failed to remove directory: {:?}", e);
-                    }
-                } else {
-                    if let Err(e) = fs::remove_file(&entry_path) {
-                        eprintln!("Failed to remove file: {:?}", e);
-                    }
-                }
-            }
-        });
-    }
-    Ok(())
+fn init_temp_dir() -> (TempDir, PathBuf, String) {
+    let name = "plugin_test";
+    let temp_dir = Builder::new()
+        .prefix(name)
+        .tempdir()
+        .expect("Failed to create temp dir");
+    let temp_dir_path = temp_dir.path().to_path_buf();
+
+    println!("temp_dir: {:?}", temp_dir);
+    println!("temp_dir_path: {:?}", temp_dir_path);
+    println!("temp_dir_name: {:?}", name);
+
+    (temp_dir, temp_dir_path, name.to_string())
 }
