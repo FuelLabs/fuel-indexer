1cd2f795b74a3bfff6ca7ca8f6837a4d22258fcc
rashad <spam.rashad@protonmail.com>
2023-03-21 10:41:34 -0600

    chore: refactor codegen (#625)
    * enhancement: add vec support
    
    * enhancement: ability to get function name from Call receipt
    
    * update contract id
    
    ---------
    
    Co-authored-by: Rashad Alston <rashad@Rashads-MacBook-Pro.local>
    Co-authored-by: Alexander Decurnou <alex@deekerno.com>

--- packages/fuel-indexer-tests/src/fixtures.rs
+++ packages/fuel-indexer-tests/src/fixtures.rs
@@ -282,7 +282,6 @@ pub mod test_web {
         signers::WalletUnlocked,
     };
     use std::path::Path;
-
     async fn fuel_indexer_test_blocks(state: web::Data<Arc<AppState>>) -> impl Responder {
         let _ = state
             .contract
@@ -496,6 +495,47 @@ pub mod test_web {
         HttpResponse::Ok()
     }
 
+    async fn fuel_indexer_vec_calldata(
+        state: web::Data<Arc<AppState>>,
+    ) -> impl Responder {
+        let _ = state
+            .contract
+            .methods()
+            .trigger_vec_pong_calldata(vec![1, 2, 3, 4, 5, 6, 7, 8])
+            .tx_params(tx_params())
+            .call()
+            .await
+            .unwrap();
+        HttpResponse::Ok()
+    }
+
+    async fn fuel_indexer_vec_logdata(state: web::Data<Arc<AppState>>) -> impl Responder {
+        let _ = state
+            .contract
+            .methods()
+            .trigger_vec_pong_logdata()
+            .tx_params(tx_params())
+            .call()
+            .await
+            .unwrap();
+        HttpResponse::Ok()
+    }
+
+    async fn fuel_indexer_test_pure_function(
+        state: web::Data<Arc<AppState>>,
+    ) -> impl Responder {
+        let _ = state
+            .contract
+            .methods()
+            .trigger_pure_function()
+            .tx_params(tx_params())
+            .call()
+            .await
+            .unwrap();
+
+        HttpResponse::Ok()
+    }
+
     pub struct AppState {
         pub contract: FuelIndexerTest,
     }
@@ -548,6 +588,12 @@ pub mod test_web {
                 web::post().to(
                     fuel_indexer_test_nested_query_explicit_foreign_keys_schema_fields,
                 ),
+            )
+            .route("/vec-calldata", web::post().to(fuel_indexer_vec_calldata))
+            .route("/vec-logdata", web::post().to(fuel_indexer_vec_logdata))
+            .route(
+                "/pure_function",
+                web::post().to(fuel_indexer_test_pure_function),
             )
     }
 
