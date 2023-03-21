db9ba74b594d34ae31d7c963a63186b3eec333e2
rvmelkonian <r.v.melkonian@gmail.com>
2023-03-21 13:10:28 +0000

    add test for new and refactor

--- plugins/plugin-tests/tests/commands.rs
+++ plugins/plugin-tests/tests/commands.rs
@@ -1,8 +1,7 @@
 use forc_index::commands::{
     build::Command as BuildCommand, init::Command as InitCommand,
+    new::Command as NewCommand,
 };
-use std::fs::{self, ReadDir};
-use std::io;
 use std::path::{Path, PathBuf};
 use tempfile::{Builder, TempDir};
 
@@ -10,85 +9,66 @@ use tempfile::{Builder, TempDir};
 fn init_command_creates_correct_project_tree() {
     let (_temp_dir, temp_dir_path, temp_dir_name) = init_temp_dir();
 
-    let command = InitCommand {
+    forc_index::commands::init::exec(InitCommand {
         name: Some(temp_dir_name.clone()),
         path: Some(temp_dir_path.clone()),
         namespace: temp_dir_name.to_string(),
         native: false,
         absolute_paths: false,
-    };
-
-    forc_index::commands::init::exec(command).expect("Init command failed");
+    })
+    .expect("Init command failed");
 
     let manifest = temp_dir_name.clone() + ".manifest.yaml";
     let schema_path = format!("schema/{}.schema.graphql", temp_dir_name);
 
-    let expected_project_tree = vec![
+    [
         manifest.as_str(),
         "Cargo.toml",
         "src",
         "src/lib.rs",
         "schema",
         schema_path.as_str(),
-    ];
-
-    for path in expected_project_tree {
-        let full_path = temp_dir_path.join(path);
+    ]
+    .iter()
+    .map(|path| temp_dir_path.join(path))
+    .for_each(|full_path| {
         assert!(
             full_path.exists(),
             "Expected path '{}' does not exist.",
             full_path.to_string_lossy()
         )
-    }
+    });
 }
-
-//@TODO add negative case for init command
 
 #[test]
 fn build_command_creates_artifact_at_expected_path() {
     let (_temp_dir, temp_dir_path, temp_dir_name) = init_temp_dir();
 
-    let command = InitCommand {
+    forc_index::commands::init::exec(InitCommand {
         name: Some(temp_dir_name.clone()),
         path: Some(temp_dir_path.clone()),
         namespace: temp_dir_name.to_string(),
         native: false,
         absolute_paths: false,
-    };
+    })
+    .expect("Init command failed");
 
-    forc_index::commands::init::exec(command).expect("Init command failed");
-
-    println!("init success");
-
-    //cd into the test indexer project temp dir
     std::env::set_current_dir(&temp_dir_path).expect("Failed to set current dir");
 
-    //convert temp_dir_name from camelCase to snake-case
-    let temp_dir_name = temp_dir_name
-        .chars()
-        .map(|c| {
-            if c.is_uppercase() {
-                format!("_{}", c.to_lowercase())
-            } else {
-                c.to_string()
-            }
-        })
-        .collect::<String>();
     let manifest = temp_dir_name.clone() + ".manifest.yaml";
 
-    let command = BuildCommand {
+    forc_index::commands::build::exec(BuildCommand {
         target: None,
         native: false,
         path: None,
-        verbose: true,
+        verbose: false,
         profile: None,
         release: false,
         locked: false,
         manifest: Some(manifest),
         output_dir_root: None,
-    };
-
-    forc_index::commands::build::exec(command).expect("Build command failed");
+    })
+    .expect("Build command failed");
     let wasm_artifact_path =
         temp_dir_path.join("target/wasm32-unknown-unknown/debug/plugin_test.wasm");
 
@@ -98,6 +78,49 @@ fn build_command_creates_artifact_at_expected_path() {
     );
 }
 
+#[test]
+fn new_command_initializes_project_at_new_directory() {
+    let (_temp_dir, temp_dir_path, _temp_dir_name) = init_temp_dir();
+
+    std::env::set_current_dir(&temp_dir_path).expect("Failed to set current dir");
+    let new_project_name = "new_project_dir";
+    let new_project_path = PathBuf::from(new_project_name);
+
+    forc_index::commands::new::exec(NewCommand {
+        name: Some(new_project_name.to_string()),
+        path: new_project_path.clone(),
+        namespace: new_project_name.to_string(),
+        native: false,
+        absolute_paths: false,
+    })
+    .expect("New command failed");
+
+    std::env::set_current_dir(&new_project_path).expect("Failed to set current dir");
+    let current_dir = std::env::current_dir().expect("Failed to get current dir");
+
+    let format_path = |path: &str| format!("{}/{}", current_dir.to_string_lossy(), path);
+    let manifest = format!("{}.manifest.yaml", new_project_name);
+    let schema_path = format!("schema/{}.schema.graphql", new_project_name);
+
+    [
+        manifest.as_str(),
+        "Cargo.toml",
+        "src",
+        "src/lib.rs",
+        "schema",
+        schema_path.as_str(),
+    ]
+    .iter()
+    .map(|path| PathBuf::from(format_path(path)))
+    .for_each(|full_path| {
+        assert!(
+            full_path.exists(),
+            "Expected path '{}' does not exist.",
+            full_path.to_string_lossy()
+        )
+    });
+}
+
 fn init_temp_dir() -> (TempDir, PathBuf, String) {
     let name = "plugin_test";
     let temp_dir = Builder::new()
@@ -105,10 +128,6 @@ fn init_temp_dir() -> (TempDir, PathBuf, String) {
         .tempdir()
         .expect("Failed to create temp dir");
     let temp_dir_path = temp_dir.path().to_path_buf();
-
-    println!("temp_dir: {:?}", temp_dir);
-    println!("temp_dir_path: {:?}", temp_dir_path);
-    println!("temp_dir_name: {:?}", name);
 
     (temp_dir, temp_dir_path, name.to_string())
 }
