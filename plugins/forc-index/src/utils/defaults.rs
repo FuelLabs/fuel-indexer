e6d38f0c236bee4be73fbc4ad8a29d3013934646
rashad <spam.rashad@protonmail.com>
2023-04-10 20:25:35 -0400

    v0.8.0 (#752)
    Co-authored-by: Rashad Alston <rashad@Rashads-MacBook-Air.local>

--- plugins/forc-index/src/utils/defaults.rs
+++ plugins/forc-index/src/utils/defaults.rs
@@ -22,9 +22,9 @@ publish = false
 crate-type = ['cdylib']
 
 [dependencies]
-fuel-indexer-macros = {{ version = "0.7", default-features = false }}
-fuel-indexer-plugin = {{ version = "0.7", features = ["native-execution"] }}
-fuel-indexer-schema = {{ version = "0.7", default-features = false }}
+fuel-indexer-macros = {{ version = "0.8", default-features = false }}
+fuel-indexer-plugin = {{ version = "0.8", features = ["native-execution"] }}
+fuel-indexer-schema = {{ version = "0.8", default-features = false }}
 fuel-tx = "0.26"
 fuels = {{ version = "0.38.1" }}
 fuels-core = {{ version = "0.38.1" }}
@@ -47,9 +47,9 @@ publish = false
 crate-type = ['cdylib']
 
 [dependencies]
-fuel-indexer-macros = {{ version = "0.7", default-features = false }}
-fuel-indexer-plugin = {{ version = "0.7" }}
-fuel-indexer-schema = {{ version = "0.7", default-features = false }}
+fuel-indexer-macros = {{ version = "0.8", default-features = false }}
+fuel-indexer-plugin = {{ version = "0.8" }}
+fuel-indexer-schema = {{ version = "0.8", default-features = false }}
 fuel-tx = "0.26"
 fuels-core = {{ version = "0.38.1", default-features = false }}
 fuels-macros = {{ version = "0.38.1" }}
