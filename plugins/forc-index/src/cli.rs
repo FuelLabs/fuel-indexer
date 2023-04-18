38e7f5783bc71d03a2bc41fb77468dec8e69037c
rvmelkonian <r.v.melkonian@gmail.com>
2023-04-18 15:44:20 +0100

    comment out command

--- plugins/forc-index/src/cli.rs
+++ plugins/forc-index/src/cli.rs
@@ -1,3 +1,4 @@
+#[allow(unused)]
 pub(crate) use crate::commands::{
     auth::Command as AuthCommand, build::Command as BuildCommand,
     check::Command as CheckCommand, deploy::Command as DeployCommand,
@@ -32,7 +33,7 @@ pub enum ForcIndex {
     Build(BuildCommand),
     Auth(AuthCommand),
     Postgres(ForcPostgresOpt),
-    Welcome(WelcomeCommand),
+    //Welcome(WelcomeCommand),
 }
 
 pub async fn run_cli() -> Result<(), anyhow::Error> {
@@ -51,7 +52,7 @@ pub async fn run_cli() -> Result<(), anyhow::Error> {
         ForcIndex::Remove(command) => crate::commands::remove::exec(command),
         ForcIndex::Revert(command) => crate::commands::revert::exec(command).await,
         ForcIndex::Build(command) => crate::commands::build::exec(command),
-        ForcIndex::Welcome(command) => crate::commands::welcome::exec(command).await,
+        //ForcIndex::Welcome(command) => crate::commands::welcome::exec(command).await,
         ForcIndex::Auth(command) => crate::commands::auth::exec(command),
         ForcIndex::Postgres(opt) => match opt.command {
             ForcPostgres::Create(command) => pg_commands::create::exec(command).await,
