9dd6f7471a53ac3c93132be2d88b6ac3b29e17a3
rvmelkonian <r.v.melkonian@gmail.com>
2023-04-23 14:37:33 +0100

    use optional contract_name arg, cleanup

--- plugins/forc-index/src/commands/pull_abi.rs
+++ plugins/forc-index/src/commands/pull_abi.rs
@@ -7,11 +7,11 @@ use std::path::PathBuf;
 pub struct Command {
     ////// URL of the ABI file
     #[clap(long, help = "URL of the ABI file.")]
-    pub url: String,
+    pub raw_url: String,
 
     /// Name of contract
     #[clap(long, help = "Name of contract.")]
-    pub contract_name: String,
+    pub contract_name: Option<String>,
 
     /// Path at which to write the ABI.
     #[clap(
@@ -28,6 +28,6 @@ pub struct Command {
 }
 
 pub async fn exec(command: Command) -> Result<(), anyhow::Error> {
-    forc_index_pull_abi::init(command).await;
+    forc_index_pull_abi::init(command).await?;
     Ok(())
 }
