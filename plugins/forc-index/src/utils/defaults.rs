685c2aad6b7303c5bdd5faa7f02cfc8b62eb7b39
Alexander <alex@deekerno.com>
2023-04-23 16:08:16 -0400

    Fix index plugins failure

--- plugins/forc-index/src/utils/defaults.rs
+++ plugins/forc-index/src/utils/defaults.rs
@@ -53,9 +53,7 @@ fuel-indexer-macros = {{ version = "0.10", default-features = false }}
 fuel-indexer-plugin = {{ version = "0.10" }}
 fuel-indexer-schema = {{ version = "0.10", default-features = false }}
 fuel-tx = "0.26"
-fuels-core = {{ version = "0.40.0", default-features = false }}
-fuels-macros = {{ version = "0.40.0" }}
-fuels-types ={{ version = "0.40.0", default-features = false }}
+fuels = {{ version = "0.40.0", default-features = false }}
 getrandom = {{ version = "0.2", features = ["js"] }}
 serde = {{ version = "1.0", default-features = false, features = ["derive"] }}
 "#
