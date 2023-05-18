9e31334699188fc87d2520a0bdc81b679e48cf8d
Rashad Alston <rashad@Rashads-MacBook-Air.local>
2023-05-18 12:46:53 -0400

    remove old stuff

--- examples/fuel-explorer/fuel-explorer/scripts/dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Check if command exists
-if !(command -v "forc-index" >/dev/null 2>&1) ; then
-    echo "'forc index' is not installed. https://install.fuel.network/latest"
-    exit 1;
-fi
-
-pnpm exec nodemon --config ./nodemon.json
