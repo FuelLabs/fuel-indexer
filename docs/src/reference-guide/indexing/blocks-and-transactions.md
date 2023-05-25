b041c9772eeaf60f304eddb20167ed27b81b48b3
Rashad Alston <rashad@Rashads-MacBook-Air.local>
2023-05-25 11:14:22 -0400

    add indexer skeleton

--- docs/src/reference-guide/indexing/blocks-and-transactions.md
+++ docs/src/reference-guide/indexing/blocks-and-transactions.md
@@ -41,10 +41,10 @@ pub enum Transaction {
 
 `Transaction` refers to the Fuel transaction entity and can be one of three distinct types: `Script`, `Create`, or `Mint`. Explaining the differences between each of the types is out of scope for the Fuel indexer; however, you can find information about the `Transaction` type in the [Fuel specifications](https://github.com/FuelLabs/fuel-specs/blob/master/src/protocol/tx_format/transaction.md).
 
-### `TmpClientTransactionStatus`
+### `TransactionStatus`
 
 ```rust,ignore
-pub enum TmpClientTransactionStatus {
+pub enum TransactionStatus {
     Failure {
         block_id: String,
         time: DateTime<Utc>,
@@ -63,4 +63,4 @@ pub enum TmpClientTransactionStatus {
 }
 ```
 
-`TmpClientTransactionStatus` refers to the status of a `Transaction` in the Fuel network.
+`TransactionStatus` refers to the status of a `Transaction` in the Fuel network.
--- examples/fuel-explorer/fuel-explorer/src/lib.rs
+++ examples/fuel-explorer/fuel-explorer/src/lib.rs
@@ -71,35 +71,92 @@ impl From<ConsensusData> for Consensus {
 #[indexer(manifest = "examples/fuel-explorer/fuel-explorer/fuel_explorer.manifest.yaml")]
 pub mod explorer_index {
 
-    fn index_block(block: BlockData) {
+    fn index_block(block_data: BlockData) {
         let header = Header {
             id: 1,
-            block_id: block.header.id,
-            da_height: block.header.da_height,
-            transactions_count: block.header.transactions_count,
-            message_receipt_count: block.header.output_messages_count,
-            transactions_root: block.header.transactions_root,
-            message_receipt_root: block.header.output_messages_root,
-            height: block.header.height,
-            prev_root: block.header.prev_root,
-            time: block.header.time,
-            application_hash: block.header.application_hash,
+            block_id: block_data.header.id,
+            da_height: block_data.header.da_height,
+            transactions_count: block_data.header.transactions_count,
+            message_receipt_count: block_data.header.output_messages_count,
+            transactions_root: block_data.header.transactions_root,
+            message_receipt_root: block_data.header.output_messages_root,
+            height: block_data.header.height,
+            prev_root: block_data.header.prev_root,
+            time: block_data.header.time,
+            application_hash: block_data.header.application_hash,
         };
 
         header.save();
 
-        let consensus = Consensus::from(block.consensus);
+        let consensus = Consensus::from(block_data.consensus);
         consensus.save();
 
         let block = Block {
             id: 1,
-            block_id: block.header.id,
+            block_id: block_data.header.id,
             header: header.id,
             consensus: consensus.id,
         };
 
-        Logger::info("hello, world!");
-
+        // Save partial block
         block.save();
+
+        for transaction in block_data.transactions.iter() {
+            match &transaction.transaction {
+                #[allow(unused)]
+                ClientTransaction::Script(t) => {
+                    let gas_limit = t.gas_limit();
+                    let gas_price = t.gas_price();
+                    let maturity = t.maturity();
+                    let script = t.script();
+                    let script_data = t.script_data();
+                    let receipts_root = t.receipts_root();
+                    let inputs = t.inputs();
+                    let outputs = t.outputs();
+                    let witnesses = t.witnesses();
+                }
+                #[allow(unused)]
+                ClientTransaction::Create(t) => {
+                    let gas_limit = t.gas_limit();
+                    let gas_price = t.gas_price();
+                    let maturity = t.maturity();
+                    let salt = t.salt();
+                    let bytecode_length = t.bytecode_length();
+                    let bytecode_witness_index = t.bytecode_witness_index();
+                    let inputs = t.inputs();
+                    let outputs = t.outputs();
+                    let witnesses = t.witnesses();
+                    let storage_slots = t.storage_slots();
+                }
+                #[allow(unused)]
+                ClientTransaction::Mint(t) => {
+                    let tx_pointer = t.tx_pointer();
+                    let outputs = t.outputs();
+                }
+            }
+
+            for receipt in transaction.receipts.iter() {
+                match receipt {
+                    Receipt::Call { .. } => {}
+                    #[allow(unused)]
+                    Receipt::ReturnData { .. } => {}
+                    #[allow(unused)]
+                    Receipt::Transfer { .. } => {}
+                    #[allow(unused)]
+                    Receipt::TransferOut { .. } => {}
+                    #[allow(unused)]
+                    Receipt::Log { .. } => {}
+                    #[allow(unused)]
+                    Receipt::LogData { .. } => {}
+                    #[allow(unused)]
+                    Receipt::ScriptResult { .. } => {}
+                    #[allow(unused)]
+                    Receipt::MessageOut { .. } => {}
+                    _ => {
+                        Logger::info("This Receipt type is not handled yet.");
+                    }
+                }
+            }
+        }
     }
 }
