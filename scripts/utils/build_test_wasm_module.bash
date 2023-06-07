4c94ed80f3f154f675a5b17618f46916630b785e
Maciej Woś <maciej.wos@gmail.com>
2023-06-07 21:02:58 +0200

    remove fuel-indexer-test-redeploy test indexer

--- scripts/utils/build_test_wasm_module.bash
+++ scripts/utils/build_test_wasm_module.bash
@@ -13,12 +13,6 @@ cp fuel_indexer_test.wasm target/wasm32-unknown-unknown/release/
 rm -fv fuel_indexer_test.wasm
 
 # This is a test index, keep its assets for now
-cargo build -p fuel-indexer-test-redeploy --release --target wasm32-unknown-unknown
-bash scripts/stripper.bash fuel_indexer_test_redeploy.wasm
-cp fuel_indexer_test_redeploy.wasm target/wasm32-unknown-unknown/release/
-rm -fv fuel_indexer_test_redeploy.wasm
-
-# This is a test index, keep its assets for now
 cargo build -p simple-wasm --release --target wasm32-unknown-unknown
 bash scripts/stripper.bash simple_wasm.wasm
 cp simple_wasm.wasm target/wasm32-unknown-unknown/release/
