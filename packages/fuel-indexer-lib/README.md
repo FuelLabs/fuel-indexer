c557bbd1bae0c822d9f1e050b880a5218f9f66d0
Rashad Alston <rashad@Rashads-MacBook-Air.local>
2023-06-14 16:54:12 -0400

    update readme

--- packages/fuel-indexer-lib/README.md
+++ packages/fuel-indexer-lib/README.md
@@ -1,1 +1,12 @@
 # fuel-indexer-lib
+
+A collection of utilities used by the various `fuel-indexer-*` crates.
+
+```rust
+use fuel_indexer_lib::utils::trim_opt_env_key;
+
+fn main() {
+    let a = "${ENV_VAR_KEY}";
+    assert_eq!(trim_opt_env_key(a), "ENV_VAR_KEY");
+}
+```
--- packages/fuel-indexer-lib/src/lib.rs
+++ packages/fuel-indexer-lib/src/lib.rs
@@ -1,9 +1,8 @@
 //! # fuel-indexer-lib
 //!
-//! `fuel-indexer-lib` is a collection of utilities used by the various `fuel-indexer-*` crates.
+//! A collection of utilities used by the various `fuel-indexer-*` crates.
 
 #![deny(unused_crate_dependencies)]
-
 pub mod config;
 pub mod defaults;
 pub mod manifest;
