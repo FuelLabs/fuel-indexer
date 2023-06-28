808af5e7216b608ee4a2a23216171d3d450dbd37
Maciej Woś <maciej.wos@gmail.com>
2023-06-28 18:02:14 +0200

    forc index kill: terminate or kill (#1038)
    add option to terminate or kill fuel-indexer

--- plugins/forc-index/src/ops/forc_index_kill.rs
+++ plugins/forc-index/src/ops/forc_index_kill.rs
@@ -5,12 +5,12 @@ use tracing::info;
 pub fn kill(command: KillCommand) -> anyhow::Result<()> {
     let port_number = command.port.parse::<u16>().unwrap();
 
-    kill_process_by_port(port_number)?;
+    kill_process_by_port(port_number, command.kill)?;
 
     Ok(())
 }
 
-fn kill_process_by_port(port: u16) -> anyhow::Result<()> {
+fn kill_process_by_port(port: u16, kill: bool) -> anyhow::Result<()> {
     let output = Command::new("lsof")
         .arg("-ti")
         .arg(format!(":{}", port))
@@ -29,16 +29,18 @@ fn kill_process_by_port(port: u16) -> anyhow::Result<()> {
         .parse::<i32>()
         .map_err(|e| anyhow::anyhow!("❌ Failed to parse PID: {}", e))?;
 
-    Command::new("kill")
-        .arg("-9")
-        .arg(pid.to_string())
-        .status()
-        .map_err(|e| anyhow::anyhow!("❌ Failed to kill process: {}", e))?;
+    let signal = if kill { "kill" } else { "terminate" };
 
-    info!(
-        "✅ Sucessfully killed process {} listening on port {}",
-        pid, port
-    );
+    let mut cmd = Command::new("kill");
+    if kill {
+        cmd.arg("-9");
+    }
+    cmd.arg(pid.to_string())
+        .status()
+        .map_err(|e| anyhow::anyhow!("❌ Failed to {signal} process: {}", e))?;
+
+    let signal = if kill { "killed" } else { "terminated" };
+    info!("✅ Sucessfully {signal} process {pid} listening on port {port}");
 
     Ok(())
 }
