fa8bfad474c2bc71c9ed227dc3b94ac6e745c3aa
Maciej Woś <maciej.wos@gmail.com>
2023-07-11 23:46:42 +0200

    small tweak

--- packages/fuel-indexer/src/executor.rs
+++ packages/fuel-indexer/src/executor.rs
@@ -700,7 +700,7 @@ impl Executor for WasmIndexExecutor {
                     == Some(wasmer_types::TrapCode::UnreachableCodeReached)
                 && self.get_instance_metering_points() == MeteringPoints::Exhausted
             {
-                let _ = self.db.lock().await.revert_transaction().await?;
+                self.db.lock().await.revert_transaction().await?;
                 return Err(IndexerError::RunTimeLimitExceededError);
             } else {
                 error!("WasmIndexExecutor handle_events failed: {e:?}.");
