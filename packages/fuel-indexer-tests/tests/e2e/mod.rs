cd447b58d4a36ce309c1ee2c879a184b09b57aa3
Maciej Woś <maciej.wos@gmail.com>
2023-07-13 18:01:48 +0200

    move feature flags again

--- packages/fuel-indexer-tests/tests/e2e/mod.rs
+++ packages/fuel-indexer-tests/tests/e2e/mod.rs
@@ -1,4 +1,3 @@
-#[cfg(all(feature = "e2e", feature = "postgres"))]
 mod graphql_api_postgres;
 #[cfg(all(feature = "e2e", feature = "postgres"))]
 mod indexing_postgres;
--- packages/fuel-indexer-tests/tests/integration/mod.rs
+++ packages/fuel-indexer-tests/tests/integration/mod.rs
@@ -1,5 +1,4 @@
 mod database;
 mod graphql_schema;
 mod service;
-#[cfg(feature = "postgres")]
 mod web_api_postgres;
