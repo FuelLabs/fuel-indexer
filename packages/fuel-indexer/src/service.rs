40f9a3d997d8036ba3900b9a6fa60f254f5ccc3c
Maciej Woś <maciej.wos@gmail.com>
2023-08-16 17:17:53 +0200

    test get_start_block

--- packages/fuel-indexer/src/service.rs
+++ packages/fuel-indexer/src/service.rs
@@ -357,7 +357,7 @@ async fn create_service_task(
 }
 
 /// Determine the starting block for this indexer.
-async fn get_start_block(
+pub async fn get_start_block(
     conn: &mut IndexerConnection,
     manifest: &Manifest,
 ) -> Result<u32, IndexerError> {
