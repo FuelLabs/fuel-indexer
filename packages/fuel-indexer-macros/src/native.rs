c7d9f974d7483737c64da645ecc120b6a6e3d29d
rashad <spam.rashad@protonmail.com>
2023-09-11 14:16:16 -0400

    enhancement: add support for sway generics (#1332)
    * enhancement: add support for sway generics
    
    updates
    
    * lostman feedback
    
    ---------
    
    Co-authored-by: Rashad Alston <rashad@Rashads-Air.lan>

--- packages/fuel-indexer-macros/src/native.rs
+++ packages/fuel-indexer-macros/src/native.rs
@@ -26,7 +26,7 @@ pub fn handler_block_native(
     }
 }
 
-/// Prelude imports for the _indexer_ module.
+/// Prelude imports for the `indexer` module.
 ///
 /// These imports are placed below the top-level lib imports, so any
 /// dependencies imported here will only be within the scope of the
@@ -45,5 +45,58 @@ fn native_prelude() -> proc_macro2::TokenStream {
             core::{codec::ABIDecoder, Configurables, traits::{Parameterize, Tokenizable}},
             types::{StringToken},
         };
+    }
+}
+
+/// Generate the `main` function for the native execution module.
+pub fn native_main() -> proc_macro2::TokenStream {
+    quote! {
+        #[tokio::main]
+        async fn main() -> anyhow::Result<()> {
+
+            let args = IndexerArgs::parse();
+
+            let IndexerArgs { manifest, .. } = args.clone();
+
+
+            let config = args
+            .config
+            .as_ref()
+            .map(IndexerConfig::from_file)
+            .unwrap_or(Ok(IndexerConfig::from(args)))?;
+
+            init_logging(&config).await?;
+
+            info!("Configuration: {:?}", config);
+
+            let (tx, rx) = channel::<ServiceRequest>(SERVICE_REQUEST_CHANNEL_SIZE);
+
+            let pool = IndexerConnectionPool::connect(&config.database.to_string()).await?;
+
+            if config.run_migrations {
+                let mut c = pool.acquire().await?;
+                queries::run_migration(&mut c).await?;
+            }
+
+            let mut service = IndexerService::new(config.clone(), pool.clone(), rx).await?;
+
+            if manifest.is_none() {
+                panic!("Manifest required to use native execution.");
+            }
+
+            let p = manifest.unwrap();
+            if config.verbose {
+                info!("Using manifest file located at '{}'", p.display());
+            }
+            let manifest = Manifest::from_file(&p)?;
+            service.register_native_indexer(manifest, handle_events).await?;
+
+            let service_handle = tokio::spawn(service.run());
+            let web_handle = tokio::spawn(WebApi::build_and_run(config.clone(), pool, tx));
+
+            let _ = tokio::join!(service_handle, web_handle);
+
+            Ok(())
+        }
     }
 }
