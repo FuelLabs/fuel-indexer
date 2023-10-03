1fbc99d12c871666fbf8b2f74cfe77016555c050
ra0x3 <spam.rashad@protonmail.com>
2023-10-03 17:48:40 -0400

    remove native

--- packages/fuel-indexer-tests/scripts/test-examples.bash
+++ packages/fuel-indexer-tests/scripts/test-examples.bash
@@ -62,12 +62,14 @@ forc_index_kill
 # examples/greetings-native
 # ********************************
 
-cd ../greetings-native
+# cd ../greetings-native
 
-forc-index build --path ./greetings-native-indexer --native
+# forc-index build --path ./greetings-native-indexer --native
 
-# Update manifest with paths relative to the example due to cargo workspace
-sed -i "s|^graphql_schema: examples/greetings-native/greetings-native-indexer/schema/greetings_native_indexer\.schema\.graphql|graphql_schema: $PWD/greetings-native-indexer/schema/greetings_native_indexer.schema.graphql|" ./greetings-native-indexer/greetings_native_indexer.manifest.yaml
+# # Update manifest with paths relative to the example due to cargo workspace
+# sed -i \
+#   "s|^graphql_schema: examples/greetings-native/greetings-native-indexer/schema/greetings_native_indexer\.schema\.graphql|graphql_schema: $PWD/greetings-native-indexer/schema/greetings_native_indexer.schema.graphql|" \
+#     ./greetings-native-indexer/greetings_native_indexer.manifest.yaml
 
 # Start indexer in the background
 # forc-index run-native --path ./greetings-native-indexer -- --run-migrations --postgres-password my-secret
@@ -82,7 +84,7 @@ sed -i "s|^graphql_schema: examples/greetings-native/greetings-native-indexer/sc
 # echo "SELECT COUNT(*) FROM fuellabs_greetings_native.salutation;" | psql -h localhost -p 5432 -U postgres
 # sleep 2
 
-forc_index_kill
+# forc_index_kill
 
 # ********************************
 # examples/fuel-explorer
