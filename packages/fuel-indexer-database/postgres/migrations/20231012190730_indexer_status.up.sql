1df249487217047ceecdd4af71c0ff62446c18fe
Maciej Woś <maciej.wos@gmail.com>
2023-10-18 20:35:08 +0100

    ensure status is deleted when indexer is removed

--- packages/fuel-indexer-database/postgres/migrations/20231012190730_indexer_status.up.sql
+++ packages/fuel-indexer-database/postgres/migrations/20231012190730_indexer_status.up.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS index_status (
    indexer_id BIGSERIAL UNIQUE NOT NULL,
    status TEXT NOT NULL,
    status_message TEXT NOT NULL,
-   FOREIGN KEY (indexer_id) REFERENCES index_registry (id)
+   FOREIGN KEY (indexer_id) REFERENCES index_registry (id) ON DELETE CASCADE
 );
